e3f71ed2b42a3b57029890865acf5ce5ed010331
rakeshkodapathri <rakesh.kodapathri@gmail.com>
2024-06-19 09:04:39 -0400

    bug fixes

--- rakeshapp/test/app.test.js
+++ rakeshapp/test/app.test.js
@@ -4,4 +4,6 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   // checks if the screen has the element "learn react " and stores the boolean result in linkElement
- 
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
